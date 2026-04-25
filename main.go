b9b06d309ac0b6554207cb965030032474ae4635
Thales Meier <meier.thales@gmail.com>
2026-04-25 01:05:04 -0300

    chore: update

--- main.go
+++ main.go
@@ -7,10 +7,32 @@ import (
 	tea "charm.land/bubbletea/v2"
 )
 
+type model struct {
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	return tea.NewView("don")
+}
+
 func main() {
-	p := tea.NewProgram(initialHomePageModel())
+	p := tea.NewProgram(model{})
 	if _, err := p.Run(); err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("Oops! %v", err)
 		os.Exit(1)
 	}
 }
