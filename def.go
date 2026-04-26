9e99a3b5c14be8ee8e8c390c5c5eb76bb44dc4d3
Thales Meier <meier.thales@gmail.com>
2026-04-26 09:21:37 -0300

    refactor: follow page pattern

--- def.go
+++ def.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strings"
-
 	tea "charm.land/bubbletea/v2"
 )
 
@@ -12,57 +10,4 @@ type AppModel interface {
 	Description() string
 	Children() []AppModel
 	Previous() tea.Model
-}
-
-type BucketsPage struct{}
-
-func (bp BucketsPage) Title() string {
-	return "Buckets"
-}
-
-func (bp BucketsPage) Description() string {
-	return "Buckets are the main aggregators for your finances."
-}
-
-func (bp BucketsPage) Children() []AppModel {
-	return []AppModel{}
-}
-
-func (bp BucketsPage) Previous() tea.Model {
-	return HomePage()
-}
-
-func (bp BucketsPage) Init() tea.Cmd {
-	return nil
-}
-
-func (bp BucketsPage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyPressMsg:
-		switch msg.String() {
-		case "left", "p":
-			return bp.Previous(), bp.Previous().Init()
-		case "ctrl+c", "q":
-			return bp, tea.Quit
-		}
-	}
-	return bp, nil
-}
-
-func (bp BucketsPage) View() tea.View {
-	var sb strings.Builder
-
-	sb.WriteString(UIComponentAppTitle)
-
-	ad := AppDescription{
-		content: bp.Description(),
-	}
-
-	sb.WriteString(ad.Render())
-	sb.WriteString(UIComponentNavigationInstructions)
-
-	v := tea.NewView(sb.String())
-	v.AltScreen = true
-
-	return v
 }
--- main.go
+++ main.go
@@ -3,96 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
-	"strings"
 
 	tea "charm.land/bubbletea/v2"
-	"charm.land/lipgloss/v2"
 )
-
-type model struct {
-	cursor   int
-	children []AppModel
-	width    int
-	height   int
-}
-
-func (m model) Init() tea.Cmd {
-	return tea.RequestWindowSize
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		log.Printf("window size: W %d H %d", msg.Width, msg.Height)
-		m.width = msg.Width
-		m.height = msg.Height
-	case tea.KeyPressMsg:
-		switch msg.String() {
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down", "j":
-			if m.cursor < len(m.children)-1 {
-				m.cursor++
-			}
-		case "enter", "space":
-			if len(m.children) == 0 { // No registered children, do nothing
-				return m, nil
-			}
-			choice := m.children[m.cursor]
-			return choice, choice.Init()
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		}
-	}
-	return m, nil
-}
-
-func (m model) View() tea.View {
-	var sb strings.Builder
-
-	balance := Balance{
-		currency: "USD",
-		amount:   11653200, // TODO: Query actual value
-	}
-
-	comp := lipgloss.NewCompositor(
-		lipgloss.NewLayer(UIComponentAppTitle).X(0).Z(1),
-		lipgloss.NewLayer(balance.Render()).X(m.width-lipgloss.Width(balance.Render())).Z(2),
-	)
-
-	sb.WriteString(comp.Render())
-
-	for i, child := range m.children {
-		r := Row{
-			content:    child.Title(),
-			isSelected: false,
-		}
-		if m.cursor == i {
-			r.isSelected = true
-		}
-		sb.WriteString(r.Render())
-	}
-
-	sb.WriteString(UIComponentExitInstructions)
-
-	v := tea.NewView(sb.String())
-	v.AltScreen = true
-
-	return v
-}
-
-func HomePage() model {
-	bp := BucketsPage{}
-	baseChildren := []AppModel{bp}
-	return model{
-		cursor:   0,
-		children: baseChildren,
-	}
-}
 
 func main() {
 	debug := flag.Bool("debug", false, "Wether to write debug logs to tmp/debug.log")
@@ -105,7 +19,7 @@ func main() {
 		}
 		defer f.Close()
 	}
-	p := tea.NewProgram(HomePage())
+	p := tea.NewProgram(GetHomePage())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Oops! %v", err)
 		os.Exit(1)
