5f5cb81ef0f76cb34c4d3450607e4cb4f66c4016
Thales Meier <meier.thales@gmail.com>
2026-05-14 23:39:50 -0300

    chore: update

--- cmd/config.go
+++ cmd/config.go
@@ -23,60 +23,39 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-// balanceCmd represents the balance command
-var balanceCmd = &cobra.Command{
-	Use:   "balance",
-	Short: "Get the balance of the ledger, either total or per resource",
-	Run:   RunBalance,
+// configCmd represents the config command
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Edit the configuration file",
+	Run: func(cmd *cobra.Command, args []string) {
+		nvimCmd := exec.Command("nvim", viper.ConfigFileUsed())
+		nvimCmd.Stdin = os.Stdin
+		nvimCmd.Stdout = os.Stdout
+		nvimCmd.Stderr = os.Stderr
+
+		if err := nvimCmd.Run(); err != nil {
+			fmt.Println(err)
+		}
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(balanceCmd)
+	rootCmd.AddCommand(configCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// balanceCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// balanceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	balanceCmd.Flags().StringP("resource", "r", "", "Filter by resource")
-}
-
-func RunBalance(cmd *cobra.Command, args []string) {
-	resource, _ := cmd.Flags().GetString("resource")
-	total := 0.0
-
-	ledger, err := loadLedger()
-	if err != nil {
-		panic(err)
-	}
-
-	if resource == "" {
-		total = ledger.Total()
-		fmt.Printf("Total: %.2f\n\n", total)
-
-		resources := ledger.Resources()
-		if len(resources) == 0 {
-			return
-		}
-		fmt.Printf("Detailed balances per resource:\n\n")
-		for _, r := range resources {
-			resTotal, _ := ledger.TotalByResource(r)
-			fmt.Printf("%s: %.2f\n", r, resTotal)
-		}
-	} else {
-		total, err = ledger.TotalByResource(resource)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Total for %s: %.2f\n", resource, total)
-	}
-
+	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
